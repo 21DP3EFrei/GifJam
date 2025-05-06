81b6b775508dd2ec4e288ef4f3f2aec5e3a4f713
Edgrrr <13DPEFreimanis@rvt.lv>
2025-05-06 19:11:08 +0300

    thos one for sure

--- app/Http/Controllers/SoundCategoryController.php
+++ app/Http/Controllers/SoundCategoryController.php
@@ -3,22 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Skana_Kategorija;
+use App\Models\Skana_kategorija;
 
 class SoundCategoryController extends Controller
 {
     //Page that displays all categories
     public function index()
     {
-        $SoundCategory = Skana_Kategorija::all();
-        $SoundCategory = Skana_Kategorija::with('parent')->get();
+        $SoundCategory = Skana_kategorija::all();
+        $SoundCategory = Skana_kategorija::with('parent')->get();
     
         return view('sound-categories.index', compact('SoundCategory'));
     }
     // Function to display the form for creating a new category
     public function create()
     {
-        $SoundCategory = Skana_Kategorija::all();
+        $SoundCategory = Skana_kategorija::all();
         return view('sound-categories.create', compact('SoundCategory'));
     }
 
@@ -31,7 +31,7 @@ class SoundCategoryController extends Controller
             'Apakskategorija' => 'nullable|exists:skanas_kategorija,SKat_ID'
         ]);
 
-        Skana_Kategorija::create([
+        Skana_kategorija::create([
             'Nosaukums' => $request->Kat_Nosaukums,
             'Apraksts' => $request->Apraksts, 
             'Apakskategorija' => $request->Apakskategorija,
@@ -41,15 +41,15 @@ class SoundCategoryController extends Controller
     }
 
     // Function to display a form for editing a category
-    public function edit(Skana_Kategorija $SoundCategory)
+    public function edit(Skana_kategorija $SoundCategory)
     {
-        $allSoundCategories = Skana_Kategorija::all(); 
+        $allSoundCategories = Skana_kategorija::all(); 
         return view('sound-categories.edit', compact('SoundCategory', 'allSoundCategories'));
     }
     
 
     // Function to update the edited category
-    public function update(Request $request, Skana_Kategorija $SoundCategory)
+    public function update(Request $request, Skana_kategorija $SoundCategory)
     {
         $request->validate([
             'Kat_Nosaukums' => 'required|string|max:100',
@@ -67,7 +67,7 @@ class SoundCategoryController extends Controller
     }
 
     // Function to delete a category
-    public function destroy(Skana_Kategorija $SoundCategory)
+    public function destroy(Skana_kategorija $SoundCategory)
     {
         $SoundCategory->delete();
 
